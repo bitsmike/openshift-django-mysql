7dc04bc5be8fd020ff78acf4d7dfd283cf8dd324
Bitsmike <bitsmike@gmail.com>
2015-09-04 04:31:25 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from setuptools import setup
 
 setup(
-    name='YourAppName',
+    name='tiendasmicami',
     version='1.0',
     description='OpenShift App',
     author='Edwin Michael Marin',
     author_email='bitsmike@gmail.com',
     url='http://www.python.org/sigs/distutils-sig/',
-    install_requires=['Django>=1.7'],
+    install_requires=['Django>=1.8'],
 )
